c421cbb80c8ef00246ba45f44be46e5085613eb8
PauloStraforini <paulostraforini@gmail.com>
2026-01-22 11:41:54 -0300

    Iniciando a Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 
 export default function Home() {
   return (
-    <h1 className="text-accent-blue-light">
-      Hello world
-    </h1>
+    <div className=" bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-medium text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
