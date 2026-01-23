8f332ee845a33e94e34f740013a9e2d28d9745ea
PauloStraforini <paulostraforini@gmail.com>
2026-01-23 11:52:53 -0300

    Criando o AppointmentCard

--- src/components/period-section/period-section.tsx
+++ src/components/period-section/period-section.tsx
@@ -1,5 +1,6 @@
 import { AppointmentPeriod } from '@/types/appointment';
 import { Cloudy, Moon, Sun } from 'lucide-react';
+import { AppointmentCard } from '../appointment-card';
 
 type PeriodSectionProps = {
     period: AppointmentPeriod;
@@ -27,21 +28,20 @@ export const PeriodSection = ({ period }: PeriodSectionProps) => {
             </div>
 
             {period.appointments.length > 0 ? (
-                <div vocab='px-5'>
+                <div className="px-5">
                     <div>
-                        <div className='grid grid-cols-2 md:hidden text-label-small-size text-content-secondary mb-2'>
-                            <div className='text-left'>Horario</div>
-                            <div className='text-right'>Paciente</div>
+                        <div className="grid grid-cols-2 md:hidden text-label-small-size text-content-secondary mb-2">
+                            <div className="text-left">Horário</div>
+                            <div className="text-right">Paciente</div>
                         </div>
+
                         {period.appointments.map((appointment, index) => (
-                            <div key={index}>
-                                {appointment.petName}
-                            </div>
+                            <AppointmentCard key={index} appointment={appointment} />
                         ))}
                     </div>
                 </div>
             ) : (
-                <p>Nenhum agendamento para este periodo</p>
+                <p>Nenhum agendamento para este período</p>
             )}
         </section>
     );
