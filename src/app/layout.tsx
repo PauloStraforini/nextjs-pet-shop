ef2f7ad4a0c63d54aec8f4b25090d191774f6b60
PauloStraforini <paulostraforini@gmail.com>
2026-01-29 13:24:11 -0300

    Ajustes Finais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '@/styles/globals.css';
 import { Toaster } from 'sonner';
+import '@/styles/globals.css';
+import { Header } from '@/components/header';
 
 const inter = Inter({
   variable: '--font-inter',
@@ -16,7 +17,7 @@ const interTight = Inter({
 });
 
 export const metadata: Metadata = {
-  title: 'Pet-Shop',
+  title: 'Mundo Pet',
   description:
     'Aqui você pode ver todos os clientes e serviços agendados para hoje.',
 };
@@ -29,8 +30,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${interTight.variable} antialiased`}>
-        {children}
-        <Toaster position="top-right" />
+        <Header />
+
+        <div className="max-w-3xl mx-auto">
+          <main className="flex-1 flex flex-col mt-12">
+            {children}
+            <Toaster position="top-right" />
+          </main>
+        </div>
       </body>
     </html>
   );
