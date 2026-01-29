f5fb4b0bb37067e82af60526ebb9ad935fec78b7
PauloStraforini <paulostraforini@gmail.com>
2026-01-29 13:43:53 -0300

    update error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,11 @@
-import { PrismaClient } from '@/generated/prisma';
+import { PrismaClient } from '../generated/prisma' // Ajuste o caminho conforme necessário
 
-export const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
